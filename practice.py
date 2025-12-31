c9603fac799af06035fff3814daa7be5298ecbbf
narendra <narendrasundara369@gmail.com>
2025-12-31 20:44:20 +0530

    added pattren

---
+++ practice.py
@@ -0,0 +1,16 @@
+n=int(input())
+for i in range(0,n):
+    for j in range(0,n):
+        if(j<i):
+            print(" ",end=" ")
+        else:
+            if(j%2==0):
+               print("@",end=" ")
+            else:
+               print("%",end=" ")
+    print()
+
+
+m=int(input())
+for i in range(m):
+    print(" "*i+"*"*(m-i))
